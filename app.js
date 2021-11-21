ef06c63c912a8648566efce9c369d7292002da1f
unknown <anhcuonggt@gmail.com>
2021-11-21 19:51:56 +0700

    fix lai toan bo file app.js

--- app.js
+++ app.js
@@ -4,16 +4,6 @@ const getElmClassDisplayText1 = $(".display-text1");
 const getElmClassDisplayText2 = $(".display-text2");
 const getElmClassScreenDisplayText = $(".screen__display-text");
 //
-const getElmClassBtn0 = $(".btn__0");
-const getElmClassBtn1 = $(".btn__1");
-const getElmClassBtn2 = $(".btn__2");
-const getElmClassBtn3 = $(".btn__3");
-const getElmClassBtn4 = $(".btn__4");
-const getElmClassBtn5 = $(".btn__5");
-const getElmClassBtn6 = $(".btn__6");
-const getElmClassBtn7 = $(".btn__7");
-const getElmClassBtn8 = $(".btn__8");
-const getElmClassBtn9 = $(".btn__9");
 const getElmClassBtnDivide = $(".btn__divide");
 const getElmClassBtnMultiply = $(".btn__multiply");
 const getElmClassBtnSubtract = $(".btn__subtract");
@@ -22,133 +12,108 @@ const getElmClassBtnSum = $(".btn__sum");
 const getElmClassBtnDot = $(".btn__dot");
 //
 let calculation = "";
-let fixCalCulation = "";
 let checkON = false;
 function OnOffCalculator() {
   getElmBtnOnOff.click(function () {
     getElmClassScreenDisplayText.toggleClass("display-flex");
-    calculation = "";
-    fixCalCulation = "";
-    getElmClassDisplayText1.text("");
-    getElmClassDisplayText2.text("0");
+    resetValue();
   });
 }
+// ham reset cac gai tri
+function resetValue() {
+  getElmClassDisplayText1.text("");
+  getElmClassDisplayText2.text("0");
+  calculation = "";
+  fixCalCulation = "";
+  term = "";
+  checkResult=false;
+}
+
 // Nut AC xoa gia tri
 function clearCalculator() {
   getElmBtnCE.click(function () {
-    getElmClassDisplayText1.text("");
-    getElmClassDisplayText2.text("0");
-    calculation = "";
-    fixCalCulation = "";
+    resetValue();
   });
 }
-const getElmClassBtnNumber = [
-  getElmClassBtn0,
-  getElmClassBtn1,
-  getElmClassBtn2,
-  getElmClassBtn3,
-  getElmClassBtn4,
-  getElmClassBtn5,
-  getElmClassBtn6,
-  getElmClassBtn7,
-  getElmClassBtn8,
-  getElmClassBtn9,
-];
-const getElmBtn = [
-  getElmClassBtnDivide,
-  getElmClassBtnMultiply,
-  getElmClassBtnSubtract,
-  getElmClassBtnSum,
-];
+
+// ham hien thi noi dung phep tinh ra man hinh
+function screenShow(This) {
+  calculation += $(This).text().trim();
+  getElmClassDisplayText1.text(`${calculation}`);
+}
 // ham lay gia tri cua cac nut roi hien thi len man hinh
 let term = "";
-function getValueBtnShowScreen() {
-  for (let i = 0; i <= 9; i++) {
-    getElmClassBtnNumber[i].click(function () {
-      if (checkResult) {
-        getElmClassDisplayText1.text("");
-        getElmClassDisplayText2.text("0");
-        calculation = "";
-        fixCalCulation = "";
-        term = "";
-        checkResult = false;
-      }
-      term+= `${i}`;
-      calculation += `${i}`;
-      fixCalCulation += `${i}`;
-      getElmClassDisplayText1.text(`${calculation}`);
-    });
-  }
-  for (const value of getElmBtn) {
-    value.click(function () {
-      if (checkResult) {
-        return;
-      }
-      if (checkLogic(calculation)) {
-        term = "";
-        calculation += value.text();
-        getElmClassDisplayText1.text(`${calculation}`);
-        if (value.text() == "÷") {
-          fixCalCulation += "/";
-        } else if (value.text() == "×") {
-          fixCalCulation += "*";
-        } else {
-          fixCalCulation += value.text();
-        }
-      }
-    });
-  }
-  getElmClassBtnDot.click(function () {
-    if (checkResult) {
-        return;
-      }
-    //   kiểm tra xem gia trị phần tử hiện tại có dấu '.' chưa nếu có rồi thì return ko thêm nữa;
-    for (let i = 0; i < term.length; i++) {
-        console.log(i+term[i])
-      if (term[i] == ".") {
-        return;
-      }
+function getValueBtnShowScreen() { 
+  $(".btn").click(function () {
+    if(checkResult){
+      resetValue();
     }
-    term+= `.`;
-    calculation += getElmClassBtnDot.text();
-    getElmClassDisplayText1.text(`${calculation}`);
-    fixCalCulation += getElmClassBtnDot.text();
+    screenShow(this);
+    term += $(this).text().trim();
+  });
+  $(".btn1").click(function () {
+    if (checkLogic()) {
+      screenShow(this);
+      term = "";
+    }
+  });
+  getElmClassBtnDot.click(function () {
+    if (checkLogic()) {
+      checkLogicDot();
+    }
   });
 }
+function checkLogicDot() {
+  for (let i = 0; i < term.length; i++) {
+    if (term[i] == ".") {
+      return;
+    }
+  }
+  term += getElmClassBtnDot.text();
+  calculation += getElmClassBtnDot.text();
+  getElmClassDisplayText1.text(`${calculation}`);
+}
 // hàm check logic*,/
-function checkLogic(calculation) {
-  const lastString = calculation[calculation.length - 1];
-  const arrayCalculation = calculation.split("");
+let lastString;
+function checkLogic() {
+  lastString = calculation[calculation.length - 1];
   if (
     lastString != "+" &&
     lastString != "-" &&
-    lastString != "*" &&
-    lastString != "/" &&
     lastString != "÷" &&
     lastString != "×" &&
     lastString != "." &&
-    lastString != undefined
+    lastString != undefined&&
+    checkResult!=true
   ) {
     return true;
   }
 }
+// ham chuyen du lieu sang dang chuoi tinh toan duoc
+function convertCalCulation() {
+  let convertCalculation = 
+  calculation.replaceAll("×", "*").replaceAll("÷", "/");
+  return convertCalculation;
+}
+
 let checkResult = false;
+// Ham  hien thi ket qua
 function showResult() {
   getElmClassBtnEqual.click(function () {
     if (checkResult) {
-        return;
-      }
-    if (checkLogic(calculation)) {
+      return;
+    }
+    if (checkLogic()) {
       if (
-        eval(fixCalCulation) == "Infinity" ||
-        eval(fixCalCulation) == "-Infinity"
+        eval(convertCalCulation()) == "Infinity" ||
+        eval(convertCalCulation()) == "-Infinity"
       ) {
         getElmClassDisplayText2.text(`Không thể chia cho 0 !`);
       } else {
-        getElmClassDisplayText2.text('')
-        setTimeout(function(){
-            getElmClassDisplayText2.text(`${eval(fixCalCulation)}`);
-        },60)
+        getElmClassDisplayText2.text("");
+        setTimeout(function () {
+          getElmClassDisplayText2.text(`${eval(convertCalCulation())}`);
+        }, 60);
       }
       checkResult = true;
       term = "";
